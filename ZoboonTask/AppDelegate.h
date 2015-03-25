17bd576564dfe427cb37c84d2d0497a60edfad31
Iman Adly <imanadly@Imans-MacBook-Pro-2.local>
2015-03-25 13:41:52 +0200

    Initial Commit

---
+++ ZoboonTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZoboonTask
+//
+//  Created by Iman Adly on 3/25/15.
+//  Copyright (c) 2015 Iman Adly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
